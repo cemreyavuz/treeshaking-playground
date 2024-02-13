87573962a77d7503a3d0ea03b0234d39e168bf8f
Cüneyt Emre Yavuz <cuneyt.yavuz@cognite.com>
2024-02-13 09:42:11 +0100

    chore: simplify part-1

--- part-1/app/index.js
+++ part-1/app/index.js
@@ -1,13 +1,6 @@
 import { cube } from "./math.js";
 
-function component() {
-  const element = document.createElement("pre");
-
-  element.innerHTML = ["Hello webpack!", "5 cubed is equal to " + cube(5)].join(
-    "\n\n"
-  );
-
-  return element;
-}
-
-document.body.appendChild(component());
+document.body.innerHTML = [
+  "Hello webpack!",
+  "5 cubed is equal to " + cube(5),
+].join("\n\n");
